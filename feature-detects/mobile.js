0a0f48c150d6f33c5635fdc0340b63cf13c9b950
Galaad Linosfil <neferetheka@gmail.com>
2013-03-01 13:24:58 +0000

    Fix bugs on mobile detection
    Fix bugs on mobile detection

--- feature-detects/mobile.js
+++ feature-detects/mobile.js
@@ -2,18 +2,18 @@ Modernizr.addTest('android', function(){
     return navigator.userAgent.toLowerCase().indexOf("android") > -1;
 });
 Modernizr.addTest('ios', function(){
-    return (navigator.userAgent.match( /(iPod|iPhone|iPad)/ );
+    return navigator.userAgent.match( /(iPod|iPhone|iPad)/ );
 });
 Modernizr.addTest('wp', function(){
     return navigator.userAgent.indexOf("Windows Phone OS") > -1;
 });
 Modernizr.addTest('bb', function(){
-    return (navigator.userAgent.match( /(PlayBook|BB10)/ );
+    return navigator.userAgent.match( /(PlayBook|BB10)/ );
 });
 
 Modernizr.addTest('mobile', function(){
-    return (navigator.userAgent.match( /(PlayBook|BB10)/ ) ||
-	navigator.userAgent.indexOf("Windows Phone OS") > -1 ||
-	(navigator.userAgent.match( /(iPod|iPhone|iPad)/ ) ||
-	navigator.userAgent.toLowerCase().indexOf("android") > -1
+    return navigator.userAgent.match( /(PlayBook|BB10)/ ) ||
+        navigator.userAgent.indexOf("Windows Phone OS") > -1 ||
+        navigator.userAgent.match( /(iPod|iPhone|iPad)/ ) ||
+        navigator.userAgent.toLowerCase().indexOf("android") > -1
 });
